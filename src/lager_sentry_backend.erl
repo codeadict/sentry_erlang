63905b9a4459743c8e56e085401d7c6768fc2ee0
Dairon Medina Caro <dairon@gooee.com>
2018-03-31 21:58:50 -0400

    Added more package information

--- src/lager_sentry_backend.erl
+++ src/lager_sentry_backend.erl
@@ -1,3 +1,12 @@
 -module(lager_sentry_backend).
 
 -behaviour(gen_event).
+
+-export([
+         init/1,
+         code_change/3,
+         terminate/2,
+         handle_call/2,
+         handle_event/2,
+         handle_info/2
+        ]).
