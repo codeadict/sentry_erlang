6eb05f1c1cdb897c1696d0cc7cf43c56e0c1456c
Dairon Medina Caro <dairon@gooee.com>
2018-04-08 20:11:36 -0400

    Generate hexadecimal uuids for Sentry event_ids

--- src/utils.erl
+++ src/utils.erl
@@ -12,3 +12,16 @@ user_agent() ->
 %% @end
 unix_timestamp() ->
     os:system_time(seconds).
+
+
+%% @doc
+%% Generates and hexadecimal uuid4 used for the Sentry event_id
+%% @end
+event_id() ->
+	U0 = rand:uniform((2 bsl 32) - 1),
+	U1 = rand:uniform((2 bsl 16) - 1),
+	U2 = rand:uniform((2 bsl 12) - 1),
+	U3 = rand:uniform((2 bsl 32) - 1),
+	U4 = rand:uniform((2 bsl 30) - 1),
+    <<UUID:128>> = <<U0:32, U1:16, 4:4, U2:12, 2#10:2, U3:32, U4:30>>,
+    iolist_to_binary(io_lib:format("~32.16.0b", [UUID])).
