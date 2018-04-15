e64dfc36044ccabfd653c3e181316cffa7f0184e
Dairon Medina Caro <dairon@gooee.com>
2018-04-14 20:17:53 -0400

    raise the ego :P

--- src/utils.erl
+++ src/utils.erl
@@ -1,3 +1,17 @@
+%% Copyright 2018 Dairon Medina Caro (http://dairon.org)
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
 -module(utils).
 
 
@@ -22,3 +36,7 @@ event_id() ->
     <<U0:32, U1:16, _:4, U2:12, _:2, U3:30, U4:32>> = crypto:strong_rand_bytes(16),
     <<UUID:128>> = <<U0:32, U1:16, 4:4, U2:12, 2#10:2, U3:32, U4:30>>,
     iolist_to_binary(io_lib:format("~32.16.0b", [UUID])).
+
+
+get_env(App, Name) ->
+    ok.
