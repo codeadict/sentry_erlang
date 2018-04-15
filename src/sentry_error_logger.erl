e80c3fbe6e8633e1950cec071ea1f3569e0ee083
Dairon Medina Caro <dairon@gooee.com>
2018-04-14 22:28:24 -0400

    Create Erlang error logger event

--- src/sentry_error_logger.erl
+++ src/sentry_error_logger.erl
@@ -11,3 +11,46 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
+
+-module(sentry_error_logger).
+
+-behaviour(gen_event).
+-export([
+	init/1,
+	code_change/3,
+	terminate/2,
+	handle_call/2,
+	handle_event/2,
+	handle_info/2
+]).
+
+init(_) -> 
+    {ok, []}.
+
+handle_call({configure, NewKeys}, _State) ->
+    {ok, ok, NewKeys}.
+
+handle_event({error, _, {Pid, Format, Data}}, State) ->
+    {ok, State};
+handle_event({error_report, _, {Pid, Type, Report}}, State) ->
+    {ok, State};
+
+handle_event({warning_msg, _, {Pid, Format, Data}}, State) ->
+    {ok, State};
+
+handle_event({warning_report, _, {Pid, Type, Report}}, State) ->
+    {ok, State};
+
+handle_event(_, State) ->
+    {ok, State}.
+
+handle_info(_Msg, State) ->
+    {ok, State}.
+
+code_change(_Old, State, _Extra) ->
+    {ok, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+            
+            
